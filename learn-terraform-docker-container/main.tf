dfe11926a982e927ab7b21994e46fde5b080ea44
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-11-13 19:33:50 +0900

    変更: main.tf の各ブロックにコメントを追加

--- learn-terraform-docker-container/main.tf
+++ learn-terraform-docker-container/main.tf
@@ -1,4 +1,5 @@
 terraform {
+  # 使用するプロバイダについての設定を定義
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
@@ -7,20 +8,20 @@ terraform {
   }
 }
 
-provider "docker" {}
+provider "docker" {} # Docker プロバイダを初期化する
 
 resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
+  name         = "nginx" # Docker Hub から nginx イメージを取得する
+  keep_locally = false   # terraform destroy が実行された際にイメージが削除する
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
+  image = docker_image.nginx.image_id # 上記で定義した nginx イメージを使用してコンテナを作成する
+  name  = "tutorial"                  # コンテナに "tutorial" という名前を割り当てる
 
+  # コンテナの内部ポート80と ホストのポート8000をマッピングする
   ports {
     internal = 80
     external = 8000
   }
 }
-
