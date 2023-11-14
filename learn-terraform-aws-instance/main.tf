89b5720a9f7dcaaed006358be2af3ea2161efbf5
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-11-14 23:41:30 +0900

    変更: EC2インスタンスを建てるようにmain.tfを修正

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,30 @@
+# Terraformブロック
+terraform {
+  required_providers {
+    aws = {
+      # AWSプロバイダーのソースとバージョン
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  # このコードが実行されるために必要なTerraformの最低バージョン
+  required_version = ">= 1.2.0"
+}
+
+# プロバイダーブロック
+# 今回は AWSプロバイダーを使うためにそれに関する設定を宣言する
+provider "aws" {
+  # 使用するAWSリージョン
+  region = "us-west-2"
+}
+
+# リソースブロック
+# 今回は EC2 インスタンスを建てるのでそれに関する設定を宣言する
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
